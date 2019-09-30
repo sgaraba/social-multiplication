594cd570d6f52f01db2ce152930d5f120dc28848
sgaraba <sergiu.garaba@esempla.com>
2019-09-30 16:48:26 +0300

    init v3

---
+++ src/main/java/microservices/book/multiplication/SocialMultiplicationV3Application.java
@@ -0,0 +1,13 @@
+package microservices.book.multiplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMultiplicationV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMultiplicationV3Application.class, args);
+	}
+
+}
